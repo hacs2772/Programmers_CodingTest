da1e3a17a77e65175b1af1e28ca5da6d322e6983
JiHun Ha <107793142+hacs2772@users.noreply.github.com>
2023-09-14 15:13:22 +0900

    Create 이름이 없는 동물의 아이디_LEVEL1.sql

---
+++ SQL/IS NULL/이름이 없는 동물의 아이디_LEVEL1.sql
@@ -0,0 +1,14 @@
+SELECT ANIMAL_ID
+FROM ANIMAL_INS
+WHERE NAME IS NULL
+ORDER BY ANIMAL_ID
+
+
+#동물 보호소에 들어온 동물 중, 
+#---> FROM 테이블을 알려주고있다.
+
+#이름이 없는 채로 들어온 동물의 ID를 조회 
+#---> WHERE 조건문을 알려주고있다.
+
+#단, ID는 오름차순 정렬되어야 합니다.
+#---> 정렬방법을 나타낸다

d6e48f33b61144380234a0ddcc631a084d5c9fa4
JiHun Ha <107793142+hacs2772@users.noreply.github.com>
2023-09-21 10:50:53 +0900

    Create 아픈 동물 찾기_LEVEL1.sql

---
+++ SQL/SELECT/아픈 동물 찾기_LEVEL1.sql
@@ -0,0 +1,13 @@
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION = "Sick"
+ORDER BY ANIMAL_ID
+
+#동물 보호소에 들어온 동물 중 
+#---> FROM 테이블을 설명해주고있다
+#아픈 동물의 
+#---> 조건문을 설명해주고있다
+#아이디와 이름을 조회
+#---> SELECT문을 설명해주고있다
+#이때 결과는 아이디 순으로 조회해주세요.
+#---> 정렬방법을 설명해 주고있다

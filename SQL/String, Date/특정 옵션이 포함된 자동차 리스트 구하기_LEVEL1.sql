c3f640872e266c5a5404bf125c8774d0066657d5
JiHun Ha <107793142+hacs2772@users.noreply.github.com>
2023-11-13 13:13:52 +0900

    Create 특정 옵션이 포함된 자동차 리스트 구하기_LEVEL1.sql

---
+++ SQL/String, Date/특정 옵션이 포함된 자동차 리스트 구하기_LEVEL1.sql
@@ -0,0 +1,13 @@
+SELECT *
+FROM CAR_RENTAL_COMPANY_CAR
+WHERE OPTIONS LIKE "%네비게이션%"
+ORDER BY CAR_ID DESC
+
+#CAR_RENTAL_COMPANY_CAR 테이블에서 
+#---> FROM테이블에대해 설명해주고있다.
+#'네비게이션' 옵션이 포함된 
+#---> 조건문을 설명해주고있다.
+#자동차 리스트를 출력하는 SQL문을 작성해주세요. 
+#---> 출력해줄 SELECT부분을 설명해주고있다.
+#결과는 자동차 ID를 기준으로 내림차순 정렬해주세요.
+#---> 정렬방법에대해 설명해주고있다.
